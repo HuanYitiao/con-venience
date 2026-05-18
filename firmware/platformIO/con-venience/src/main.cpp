5841d6ace8c7a4bd17a1704e1bb09b0ff4cddf4e
HuanYitiao <85780243+HuanYitiao@users.noreply.github.com>
2026-05-18 21:56:25 +0200

    test: try GxEPD2 on board

--- firmware/platformIO/con-venience/src/main.cpp
+++ firmware/platformIO/con-venience/src/main.cpp
@@ -1,52 +1,34 @@
 #include <Arduino.h>
-#include "music.h"
-#include "songs.h"
-#include "button.h"
+#include <GxEPD2_BW.h>
+#include <Fonts/FreeMonoBold9pt7b.h>
+#include <SPI.h>
 
-#define BTN1 32
-#define BTN2 33
+SPIClass hspi(HSPI);
 
-btn_state_t BTN1State;
-btn_state_t BTN2State;
+GxEPD2_BW<GxEPD2_290_T94, GxEPD2_290_T94::HEIGHT> display(
+    GxEPD2_290_T94(/*CS=*/15, /*DC=*/27, /*RST=*/26, /*BUSY=*/25));
 
 void setup() {
-  Serial.begin(115200);
-
-  pinMode(BTN1, INPUT_PULLUP);
-  pinMode(BTN2, INPUT_PULLUP);
-
-  Serial.println("Ready.");
+    Serial.begin(115200);
+    hspi.begin(13, -1, 14, 15); // SCK, MISO, MOSI, CS
+    display.epd2.selectSPI(hspi, SPISettings(4000000, MSBFIRST, SPI_MODE0));
+    
+    display.init(115200);
+    display.clearScreen();
+    
+    display.setRotation(1);
+    display.setFont(&FreeMonoBold9pt7b);
+    display.setTextColor(GxEPD_BLACK);
+    
+    display.setFullWindow();
+    display.firstPage();
+    do {
+        display.fillScreen(GxEPD_WHITE);
+        display.setCursor(10, 30);
+        display.print("Hello fur!");
+    } while (display.nextPage());
+    
+    Serial.println("done");
 }
 
-void loop() {
-  btn_event_t BTN1Event = btn_read(BTN1, &BTN1State);
-  switch (BTN1Event)
-  {
-    case BTN_CLICK:
-      Serial.println("BTN1 click");
-      break;
-
-    case BTN_LONG_PRESS:
-    Serial.println("BTN1 long press");
-    break;
-  
-    default:
-      break;
-  }
-
-  btn_event_t BTN2Event = btn_read(BTN2, &BTN2State);
-  switch (BTN2Event)
-  {
-    case BTN_CLICK:
-      Serial.println("BTN2 click");
-      break;
-
-    case BTN_LONG_PRESS:
-    Serial.println("BTN2 long press");
-    break;
-  
-    default:
-      break;
-  }
-
-}
+void loop() {}
